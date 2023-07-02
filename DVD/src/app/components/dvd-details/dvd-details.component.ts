ef90107c5b9d2b1ebad7965b18f222ffea23f79c
BBence-dev <78971297+BBence-dev@users.noreply.github.com>
2023-07-02 10:36:12 +0200

    Update dvd-details.component.ts

--- DVD/src/app/components/dvd-details/dvd-details.component.ts
+++ DVD/src/app/components/dvd-details/dvd-details.component.ts
@@ -16,7 +16,6 @@ export class DvdDetailsComponent {
 
   @Input() viewMode = false;
 
-  // Az aktuális tutoriál, amelyet megjelenítünk vagy szerkesztünk
   @Input() currentdvd:Dvd = {
     cim: '',
     beDatum: new Date,
@@ -27,7 +26,7 @@ export class DvdDetailsComponent {
     updatedAt: ''
   };
   
-  // Üzenet, amely a műveletek eredményét jelzi
+ 
   message = '';
  Dvd: any;
 
@@ -37,16 +36,13 @@ export class DvdDetailsComponent {
     private router: Router) { }
 
   ngOnInit(): void {
-    // Az ngOnInit() metódus futása a komponens inicializálásakor
-    // Ha nem viewMode-ban vagyunk (nem csak megtekintésre szolgál a komponens), akkor az aktuális tutoriál adatainak lekérése
     if (!this.viewMode) {
       this.message = '';
       this.getdvd(this.route.snapshot.params["id"]);
     }
   }
 
-  // Az aktuális tutoriál adatainak lekérése a megadott azonosító alapján
-  getdvd(id: string): void {
+getdvd(id: string): void {
     this.dvdService.get(id)
       .subscribe({
         next: (data) => {
@@ -57,15 +53,9 @@ export class DvdDetailsComponent {
       });
   }
 
-
-  // A tutoriál frissítése az aktuális tutoriál adataival
   updatedvd(): void {
     this.message = '';
-
-    // ADvdService segítségével frissítjük a tutoriált az aktuális tutori
-    // Az Observable-t visszaadó update metódusra feliratkozunk a subscribe() segítségével
-    // Ha a kérés sikeres, a next callback függvény fut le, és üzenetet jelenít meg
-    // Ha hiba történik a kérés során, az error callback függvény fut le, és a hibát kiírja a konzolra
+    
     this.dvdService.update(this.currentdvd.id, this.currentdvd)
     .subscribe({
     next: (res) => {
@@ -76,12 +66,7 @@ export class DvdDetailsComponent {
     });
     }
     
-    // A tutoriál törlése
-    deletedvd(): void {
-    // ADvdService segítségével töröljük a tutoriált az aktuális tutoriál azonosítója alapján
-    // Az Observable-t visszaadó delete metódusra feliratkozunk a subscribe() segítségével
-    // Ha a kérés sikeres, a next callback függvény fut le, átirányítunk az '/dvds' útvonalra
-    // Ha hiba történik a kérés során, az error callback függvény fut le, és a hibát kiírja a konzolra
+  deletedvd(): void {
     this.dvdService.delete(this.currentdvd.id)
     .subscribe({
     next: (res) => {
