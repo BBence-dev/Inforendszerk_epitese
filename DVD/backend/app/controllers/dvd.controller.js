590dc2cb5ad6686ff0b16fdf7b9c7c84a48fee2c
BBence-dev <78971297+BBence-dev@users.noreply.github.com>
2023-07-02 10:28:14 +0200

    Update dvd.controller.js

--- DVD/backend/app/controllers/dvd.controller.js
+++ DVD/backend/app/controllers/dvd.controller.js
@@ -1,7 +1,7 @@
 const db = require("../models");// Az adatbázis modul importálása
-const Dvd = db.Dvds;// A Tutorial modell importálása az adatbázis modulból
+const Dvd = db.Dvds;// A  modell importálása az adatbázis modulból
 
-// Create and Save a new Tutorial
+// Create and Save a new 
 exports.create = (req, res) => {
   // Validate request - Kérés érvényességének ellenőrzése
   if (!req.body.cim) {
@@ -9,7 +9,7 @@ exports.create = (req, res) => {
     return;
   }0
 
-  // Create a Tutorial Tutorial entitás létrehozása
+  // Create a   entitás létrehozása
   const dvd = new Dvd({
     cim: req.body.cim, // A title mező értékének beállítása a kérés testéből
     beDatum: req.body.beDatum,
@@ -19,26 +19,26 @@ exports.create = (req, res) => {
    // A published mező értékének beállítása a kérés testéből, vagy alapértelmezetten false értékre
   });
 
-  // Save Tutorial in the database - Tutorial mentése az adatbázisba
+  // Save  in the database -  mentése az adatbázisba
   dvd
-    .save(dvd) // A tutorial objektum mentése az adatbázisba
+    .save(dvd) // A  objektum mentése az adatbázisba
     .then(data => {
       res.send(data);// A sikeres mentés esetén az adatok küldése a válaszban
     })
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Tutorial."
+          err.message || "Some error occurred while creating the ."
       });
     });
 };
 
-// Retrieve all Dvd from the database. - Az összes "Tutorial" entitás lekérdezése az adatbázisból
+// Retrieve all Dvd from the database. - Az összes "" entitás lekérdezése az adatbázisból
 exports.findAll = (req, res) => {
   const dvd = req.query.cim; // A kérésből származó "title" query paraméter kiolvasása
   var condition = dvd ? { cim: { $regex: new RegExp(cim), $options: "i" } } : {};
 
-  Dvd.find(condition)  // Az adatbázisból a "Tutorial" entitások lekérdezése a feltétel alapján
+  Dvd.find(condition)  // Az adatbázisból a "" entitások lekérdezése a feltétel alapján
     .then(data => {
       res.send(data);// Az adatok küldése a válaszban
     })
@@ -50,24 +50,24 @@ exports.findAll = (req, res) => {
     });
 };
 
-// Find a single Tutorial with an id - Egyetlen "Tutorial" entitás lekérdezése az azonosítója alapján
+// Find a single  with an id - Egyetlen "" entitás lekérdezése az azonosítója alapján
 exports.findOne = (req, res) => {
   const id = req.params.id; // Az azonosító kiolvasása a kérés paraméteréből
 
-  Dvd.findById(id) // Az adatbázisból a "Tutorial" entitás lekérdezése az azonosító alapján
+  Dvd.findById(id) // Az adatbázisból a "" entitás lekérdezése az azonosító alapján
     .then(data => {
       if (!data)
-        res.status(404).send({ message: "Not found Tutorial with id " + id });
+        res.status(404).send({ message: "Not found  with id " + id });
       else res.send(data); // Az adatok küldése a válaszban
     })
     .catch(err => {
       res
         .status(500)
-        .send({ message: "Error retrieving Tutorial with id=" + id });
+        .send({ message: "Error retrieving  with id=" + id });
     });
 };
 
-// Egy tutorial frissítése az azonosító alapján a kérésben
+// Egy  frissítése az azonosító alapján a kérésben
 exports.update = (req, res) => {
   if (!req.body) {
     // Ellenőrizze, hogy a kérési test nem üres
@@ -77,52 +77,52 @@ exports.update = (req, res) => {
   }
 
   const id = req.params.id;// Az azonosító kinyerése a kérés paramétereiből
- // Mongoose findByIdAndUpdate() használata a tutorial frissítéséhez az azonosító alapján
+ // Mongoose findByIdAndUpdate() használata a  frissítéséhez az azonosító alapján
   Dvd.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
     .then(data => {
       if (!data) {
-        // Ha a tutorial nem található, 404-es státuszkóddal és hibaüzenettel térünk vissza
+        // Ha a  nem található, 404-es státuszkóddal és hibaüzenettel térünk vissza
         res.status(404).send({
-          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+          message: `Cannot update  with id=${id}. Maybe  was not found!`
         });
       } else
-       // Ha a tutorial sikeresen frissítve lett, sikeres üzenettel térünk vissza
-      res.send({ message: "Tutorial was updated successfully." });
+       // Ha a  sikeresen frissítve lett, sikeres üzenettel térünk vissza
+      res.send({ message: " was updated successfully." });
     })
     .catch(err => {
       // Ha hiba történik, 500-as státuszkóddal és hibaüzenettel térünk vissza
       res.status(500).send({
-        message: "Error updating Tutorial with id=" + id
+        message: "Error updating  with id=" + id
       });
     });
 };
 
-// Egy tutorial törlése az azonosító alapján a kérésben
+// Egy  törlése az azonosító alapján a kérésben
 exports.delete = (req, res) => {
   const id = req.params.id;
 // Az azonosító kinyerése a kérés paramétereiből
-// Mongoose findByIdAndRemove() használata a tutorial törléséhez az azonosító alapján
+// Mongoose findByIdAndRemove() használata a  törléséhez az azonosító alapján
   Dvd.findByIdAndRemove(id, { useFindAndModify: false })
     .then(data => {
       if (!data) {
-        // Ha a tutorial nem található, 404-es státuszkóddal és hibaüzenettel térünk vissza
+        // Ha a  nem található, 404-es státuszkóddal és hibaüzenettel térünk vissza
         res.status(404).send({
-          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+          message: `Cannot delete  with id=${id}. Maybe  was not found!`
         });
-      } else {// Ha a tutorial sikeresen törölve lett, sikeres üzenettel térünk vissza
+      } else {// Ha a  sikeresen törölve lett, sikeres üzenettel térünk vissza
         res.send({
-          message: "Tutorial was deleted successfully!"
+          message: " was deleted successfully!"
         });
       }
     })
     .catch(err => {// Ha hiba történik, 500-as státuszkóddal és hibaüzenettel térünk vissza
       res.status(500).send({
-        message: "Could not delete Tutorial with id=" + id
+        message: "Could not delete  with id=" + id
       });
     });
 };
 
-// Az összes Tutorial törlése az adatbázisból
+// Az összes  törlése az adatbázisból
 exports.deleteAll = (req, res) => {
   Dvd.deleteMany({})
     .then(data => {
@@ -138,7 +138,7 @@ exports.deleteAll = (req, res) => {
     });
 };
 
-// Az összes publikált Tutorial lekérése
+// Az összes publikált  lekérése
 exports.findAllPublished = (req, res) => {
   Dvd.find({ condition })
     .then(data => {
