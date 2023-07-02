b3f528a7ce1a14ff07fe0d75b6b4d015a32eaaf2
BBence-dev <78971297+BBence-dev@users.noreply.github.com>
2023-07-02 10:29:14 +0200

    Update user.controller.js

--- DVD/backend/app/controllers/user.controller.js
+++ DVD/backend/app/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const db = require("../models");// Az adatbázis modul importálása
-const User = db.Users;// A Tutorial modell importálása az adatbázis modulból
+const User = db.Users;// A  modell importálása az adatbázis modulból
 
-// Create and Save a new Tutorial
+// Create and Save a new 
 exports.create = (req, res) => {
   // Validate request - Kérés érvényességének ellenőrzése
   if (!req.body.nev) {
@@ -9,7 +9,7 @@ exports.create = (req, res) => {
     return;
   }
 
-  // Create a Tutorial Tutorial entitás létrehozása
+  // Create a   entitás létrehozása
   const user = new User({
     nev: req.body.nev, // A title mező értékének beállítása a kérés testéből
     telsz: req.body.telsz,
@@ -19,26 +19,26 @@ exports.create = (req, res) => {
     condition: req.body.condition ? req.body.condition : false // A published mező értékének beállítása a kérés testéből, vagy alapértelmezetten false értékre
   });
 
-  // Save Tutorial in the database - Tutorial mentése az adatbázisba
+  // Save  in the database -  mentése az adatbázisba
   user
-    .save(user) // A tutorial objektum mentése az adatbázisba
+    .save(user) // A  objektum mentése az adatbázisba
     .then(data => {
       res.send(data);// A sikeres mentés esetén az adatok küldése a válaszban
     })
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Tutorial."
+          err.message || "Some error occurred while creating the ."
       });
     });
 };
 
-// Retrieve all User from the database. - Az összes "Tutorial" entitás lekérdezése az adatbázisból
+// Retrieve all User from the database. - Az összes "" entitás lekérdezése az adatbázisból
 exports.findAll = (req, res) => {
   const user = req.query.nev; // A kérésből származó "title" query paraméter kiolvasása
   var condition = user ? { nev: { $regex: new RegExp(nev), $options: "i" } } : {};
 
-  User.find(condition)  // Az adatbázisból a "Tutorial" entitások lekérdezése a feltétel alapján
+  User.find(condition)  // Az adatbázisból a "" entitások lekérdezése a feltétel alapján
     .then(data => {
       res.send(data);// Az adatok küldése a válaszban
     })
@@ -50,24 +50,24 @@ exports.findAll = (req, res) => {
     });
 };
 
-// Find a single Tutorial with an id - Egyetlen "Tutorial" entitás lekérdezése az azonosítója alapján
+// Find a single  with an id - Egyetlen "" entitás lekérdezése az azonosítója alapján
 exports.findOne = (req, res) => {
   const id = req.params.id; // Az azonosító kiolvasása a kérés paraméteréből
 
-  User.findById(id) // Az adatbázisból a "Tutorial" entitás lekérdezése az azonosító alapján
+  User.findById(id) // Az adatbázisból a "" entitás lekérdezése az azonosító alapján
     .then(data => {
       if (!data)
-        res.status(404).send({ message: "Not found Tutorial with id " + id });
+        res.status(404).send({ message: "Not found  with id " + id });
       else res.send(data); // Az adatok küldése a válaszban
     })
     .catch(err => {
       res
         .status(500)
-        .send({ message: "Error retrieving Tutorial with id=" + id });
+        .send({ message: "Error retrieving  with id=" + id });
     });
 };
 
-// Egy tutorial frissítése az azonosító alapján a kérésben
+// Egy  frissítése az azonosító alapján a kérésben
 exports.update = (req, res) => {
   if (!req.body) {
     // Ellenőrizze, hogy a kérési test nem üres
@@ -77,52 +77,52 @@ exports.update = (req, res) => {
   }
 
   const id = req.params.id;// Az azonosító kinyerése a kérés paramétereiből
- // Mongoose findByIdAndUpdate() használata a tutorial frissítéséhez az azonosító alapján
+ // Mongoose findByIdAndUpdate() használata a  frissítéséhez az azonosító alapján
   User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
     .then(data => {
       if (!data) {
-        // Ha a tutorial nem található, 404-es státuszkóddal és hibaüzenettel térünk vissza
+        // Ha a  nem található, 404-es státuszkóddal és hibaüzenettel térünk vissza
         res.status(404).send({
-          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+          message: `Cannot update  with id=${id}. Maybe  was not found!`
         });
       } else
-       // Ha a tutorial sikeresen frissítve lett, sikeres üzenettel térünk vissza
-      res.send({ message: "Tutorial was updated successfully." });
+       // Ha a  sikeresen frissítve lett, sikeres üzenettel térünk vissza
+      res.send({ message: " was updated successfully." });
     })
     .catch(err => {
       // Ha hiba történik, 500-as státuszkóddal és hibaüzenettel térünk vissza
       res.status(500).send({
-        message: "Error updating Tutorial with id=" + id
+        message: "Error updating  with id=" + id
       });
     });
 };
 
-// Egy tutorial törlése az azonosító alapján a kérésben
+// Egy  törlése az azonosító alapján a kérésben
 exports.delete = (req, res) => {
   const id = req.params.id;
 // Az azonosító kinyerése a kérés paramétereiből
-// Mongoose findByIdAndRemove() használata a tutorial törléséhez az azonosító alapján
+// Mongoose findByIdAndRemove() használata a  törléséhez az azonosító alapján
   User.findByIdAndRemove(id, { useFindAndModify: false })
     .then(data => {
       if (!data) {
-        // Ha a tutorial nem található, 404-es státuszkóddal és hibaüzenettel térünk vissza
+        // Ha a  nem található, 404-es státuszkóddal és hibaüzenettel térünk vissza
         res.status(404).send({
-          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+          message: `Cannot delete  with id=${id}. Maybe  was not found!`
         });
-      } else {// Ha a tutorial sikeresen törölve lett, sikeres üzenettel térünk vissza
+      } else {// Ha a  sikeresen törölve lett, sikeres üzenettel térünk vissza
         res.send({
-          message: "Tutorial was deleted successfully!"
+          message: " was deleted successfully!"
         });
       }
     })
     .catch(err => {// Ha hiba történik, 500-as státuszkóddal és hibaüzenettel térünk vissza
       res.status(500).send({
-        message: "Could not delete Tutorial with id=" + id
+        message: "Could not delete  with id=" + id
       });
     });
 };
 
-// Az összes Tutorial törlése az adatbázisból
+// Az összes  törlése az adatbázisból
 exports.deleteAll = (req, res) => {
   User.deleteMany({})
     .then(data => {
@@ -138,7 +138,7 @@ exports.deleteAll = (req, res) => {
     });
 };
 
-// Az összes publikált Tutorial lekérése
+// Az összes publikált  lekérése
 exports.findAllPublished = (req, res) => {
   User.find({ condition: true })
     .then(data => {
