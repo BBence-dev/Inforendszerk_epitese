4a3dbc039433692a70aed904e1238714cca043aa
BBence-dev <78971297+BBence-dev@users.noreply.github.com>
2023-07-02 10:30:29 +0200

    Update user.routes.js

--- DVD/backend/app/routes/user.routes.js
+++ DVD/backend/app/routes/user.routes.js
@@ -1,30 +1,30 @@
 module.exports = app => { // Express alkalmazást vár paraméterként
 
-  const user = require("../controllers/user.controller.js"); // Tutorial kontroller importálása
+  const user = require("../controllers/user.controller.js"); //  kontroller importálása
 
   var router = require("express").Router(); // Express útvonalkezelő objektum létrehozása
 
-  // Új Tutorial létrehozása
+  // Új  létrehozása
   router.post("/", user.create);
 
-  // Az összes Tutorial lekérése
+  // Az összes  lekérése
   router.get("/", user.findAll);
 
-  // Az összes publikált Tutorial lekérése
+  // Az összes publikált  lekérése
   router.get("/condition", user.findAllPublished);
 
-  // Egyetlen Tutorial lekérése az azonosítóval
+  // Egyetlen  lekérése az azonosítóval
  router.get("/:id", user.findOne);
  
   //router.get("/:id", user.getUserProduct);
 
-  // Tutorial frissítése az azonosítóval
+  //  frissítése az azonosítóval
   router.put("/:id", user.update);
 
-  // Tutorial törlése az azonosítóval
+  //  törlése az azonosítóval
   router.delete("/:id", user.delete);
 
-  // Az összes Tutorial törlése
+  // Az összes  törlése
   router.delete("/", user.deleteAll);
 
  
